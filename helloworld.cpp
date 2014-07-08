3252207f2a91b46d3ae3230ea4707222f28acd0f
idziakjakub <idziakjakub@gmail.com>
2014-07-08 10:45:07 +0200

    1000 razy witaj Krzysztof

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main(){
-	for(unsigned int i = 0; i < 100; i++)
+	for(unsigned int i = 0; i < 1000; i++)
 		if(i % 2 == 0)
-			std::cout<<"hello world"<<std::endl;
+			std::cout<<"Hello Kris"<<std::endl;
 	return 0;
 }
